ca2818c54ce5125288c8907216a23fd5b67bf2a8
Keel <soumith77@gmail.com>
2026-02-15 14:59:55 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     formats: ['image/avif', 'image/webp'],
-//   },
-//   async redirects() {
-//     return [
-//       { source: '/services/ugc', destination: '/services/ugc-video', permanent: true },
-//       { source: '/services/animation', destination: '/services/ugc-video', permanent: true },
-//     ];
-//   },
-// };
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export"
 };
 
-module.exports = nextConfig;
+export default nextConfig;
